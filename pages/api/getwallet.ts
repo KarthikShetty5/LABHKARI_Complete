09344a71cc4fc3e0b9ea86264da11b509dd14708
Karthik Shetty <karhtikshetty1@gmail.com>
2024-08-19 20:02:22 +0530

    wallet is pushed

--- pages/api/getwallet.ts
+++ pages/api/getwallet.ts
@@ -22,144 +22,141 @@ const calculatePerformanceBonus = (points: number, bv: number): number => {
   };
 
 
-const getWallet = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { userId } = req.body;
-
-  if (!userId) {
-    return res.status(400).json({
-      success: false,
-      message: "userId is required",
-    });
-  }
-
-  try {
-    const user = await User.findOne({ userId: userId });
-
-    if (!user) {
-      return res.status(404).json({
+  const getWallet = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { userId } = req.body;
+  
+    if (!userId) {
+      return res.status(400).json({
         success: false,
-        message: "User not found",
+        message: "userId is required",
       });
     }
-
-    const referralId = user.referralId;
-    const kyc = await Kyc.findOne({ userId: userId });
-
-    if (referralId && !kyc) {
-      // User has a referral and has not done KYC
-      const recentOrder = await Order.findOne({ userId: userId }).sort({
-        createdAt: -1,
-      });
-      if (recentOrder) {
-        const productId = recentOrder.productId;
-        const batch = await Batch.findOne({ productId: productId });
-        if (batch) {
-          const batchId = batch.batchNo;
-          const promo = await Promotional.findOne({ batchId: batchId }).sort({
-            applicableDate: -1,
-          });
-          if (promo) {
-            const wallet = await Wallet.findOne({ userId });
-            const cashAmount = promo.cashAmount;
-            const performanceBonus = wallet ? calculatePerformanceBonus(wallet.points, wallet.performanceBonus):0;
-
-            if (wallet) {
-              wallet.referralIncome += cashAmount;
-              wallet.promos = " ";
-              wallet.performanceBonus += performanceBonus;
-              wallet.eShopEarning += 0;
-              wallet.leaderShipBonus += 0;
-              wallet.otherFund += 0;
-              wallet.userId = referralId;
-              wallet.points += promo.points || 0;
-              await wallet.save();
-            } else {
-              const newWallet = new Wallet({
-                WalletId: new mongoose.Types.ObjectId().toString(),
-                userId: referralId,
-                referralIncome: cashAmount,
-                promos: "",
-                performanceBonus: performanceBonus,
-                eShopEarning: 0,
-                leaderShipBonus: 0,
-                otherFund: 0,
-                points: promo.points,
-              });
-              await newWallet.save();
+  
+    try {
+      const user = await User.findOne({ userId: userId });
+  
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        });
+      }
+  
+      const referralId = user.referralId;
+      const kyc = await Kyc.findOne({ userId: userId });
+  
+      if (referralId && !kyc) {
+        const recentOrder = await Order.findOne({ userId: userId }).sort({ createdAt: -1 });
+        if (recentOrder) {
+          const productId = recentOrder.productId;
+          const batch = await Batch.findOne({ productId: productId });
+          if (batch) {
+            const batchId = batch.batchNo;
+            const promo = await Promotional.findOne({ batchId: batchId }).sort({ applicableDate: -1 });
+            if (promo) {
+              const wallet = await Wallet.findOne({ userId: referralId });
+  
+              if (wallet?.processedOrderIds.includes(recentOrder._id.toString())) {
+                return res.status(200).json({
+                  success: true,
+                  message: "Order has already been processed for wallet update",
+                });
+              }
+  
+              const cashAmount = promo.cashAmount;
+              const performanceBonus = wallet ? calculatePerformanceBonus(wallet.points, wallet.performanceBonus) : 0;
+  
+              if (wallet) {
+                wallet.referralIncome += cashAmount;
+                wallet.promos = "";
+                wallet.performanceBonus += performanceBonus;
+                wallet.processedOrderIds.push(recentOrder._id.toString()); // Mark order as processed
+                wallet.points += promo.points || 0;
+                await wallet.save();
+              } else {
+                const newWallet = new Wallet({
+                  WalletId: new mongoose.Types.ObjectId().toString(),
+                  userId: referralId,
+                  referralIncome: cashAmount,
+                  promos: "",
+                  performanceBonus: performanceBonus,
+                  processedOrderIds: [recentOrder._id.toString()], // Mark order as processed
+                  points: promo.points,
+                });
+                await newWallet.save();
+              }
             }
           }
         }
+  
+        return res.status(200).json({
+          success: true,
+          message: "Calculated wallet amount and added for Referral",
+        });
       }
-
-      return res.status(200).json({
-        success: true,
-        message: "Calculated wallet amount and added for Referral",
-      });
-    }
-
-    if (kyc) {
-      // User has done KYC
-      const recentOrder = await Order.findOne({ userId: userId }).sort({
-        createdAt: -1,
-      });
-      if (recentOrder) {
-        const productId = recentOrder.productId;
-        const batch = await Batch.findOne({ productId: productId });
-        if (batch) {
-          const batchId = batch.batchNo;
-          const promo = await Promotional.findOne({ batchId: batchId }).sort({
-            applicableDate: -1,
-          });
-          if (promo) {
-            const wallet = await Wallet.findOne({ userId });
-            const cashAmount = promo.cashAmount;
-            
-            const performanceBonus = wallet ? calculatePerformanceBonus(wallet.points, wallet.performanceBonus):0;
-            if (wallet) {
-              wallet.referralIncome += cashAmount;
-              wallet.promos = " ";
-              wallet.performanceBonus += performanceBonus;
-              wallet.eShopEarning += 0;
-              wallet.leaderShipBonus += 0;
-              wallet.otherFund += 0;
-              wallet.userId = userId;
-              wallet.points += promo.points || 0;
-              await wallet.save();
-            } else {
-              const newWallet = new Wallet({
-                WalletId: new mongoose.Types.ObjectId().toString(),
-                userId: userId,
-                referralIncome: cashAmount,
-                promos: "",
-                performanceBonus: performanceBonus,
-                eShopEarning: 0,
-                leaderShipBonus: 0,
-                otherFund: 0,
-                points: promo.points,
-              });
-              await newWallet.save();
+  
+      if (kyc) {
+        const recentOrder = await Order.findOne({ userId: userId }).sort({ createdAt: -1 });
+        if (recentOrder) {
+          const productId = recentOrder.productId;
+          const batch = await Batch.findOne({ productId: productId });
+          if (batch) {
+            const batchId = batch.batchNo;
+            const promo = await Promotional.findOne({ batchId: batchId }).sort({ applicableDate: -1 });
+            if (promo) {
+              const wallet = await Wallet.findOne({ userId: userId });
+  
+              if (wallet?.processedOrderIds.includes(recentOrder._id.toString())) {
+                return res.status(200).json({
+                  success: true,
+                  message: "Order has already been processed for wallet update",
+                });
+              }
+  
+              const cashAmount = promo.cashAmount;
+              const performanceBonus = wallet ? calculatePerformanceBonus(wallet.points, wallet.performanceBonus) : 0;
+  
+              if (wallet) {
+                wallet.referralIncome += cashAmount;
+                wallet.promos = "";
+                wallet.performanceBonus += performanceBonus;
+                wallet.processedOrderIds.push(recentOrder._id.toString()); // Mark order as processed
+                wallet.points += promo.points || 0;
+                await wallet.save();
+              } else {
+                const newWallet = new Wallet({
+                  WalletId: new mongoose.Types.ObjectId().toString(),
+                  userId: userId,
+                  referralIncome: cashAmount,
+                  promos: "",
+                  performanceBonus: performanceBonus,
+                  processedOrderIds: [recentOrder._id.toString()], // Mark order as processed
+                  points: promo.points,
+                });
+                await newWallet.save();
+              }
             }
           }
         }
+  
+        return res.status(200).json({
+          success: true,
+          message: "Calculated wallet amount and added for User",
+        });
       }
-
+  
       return res.status(200).json({
         success: true,
-        message: "Calculated wallet amount and added for User",
+        message: "Amount not added anywhere",
+      });
+
+    } catch (error: any) {
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
       });
     }
-
-    return res.status(200).json({
-      success: true,
-      message: "Amount not added anywhere",
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: "Internal server error",
-      error: error.message,
-    });
-  }
 };
-
+    
 export default connectDb(getWallet);
